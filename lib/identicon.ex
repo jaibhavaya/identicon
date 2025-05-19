216653457ce60ad669e2adb48c1ddf678b3008d4
Jacob <jaibhavaya@gmail.com>
2025-05-18 22:03:23 -0600

    build grid for identicon

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,7 @@ defmodule Identicon do
     input
     |> hash_string
     |> pick_color
+    |> build_grid
   end
 
   def hash_string(input) do
@@ -15,5 +16,20 @@ defmodule Identicon do
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    rows =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+
+    %Identicon.Image{image | rows: rows}
+  end
+
+  def mirror_row(row) do
+    [first, second | _] = row
+
+    row ++ [second, first]
   end
 end
